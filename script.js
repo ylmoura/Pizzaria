f1a61544137708a0e2eb94d8fc0b61341a78448f
Yves Luan do Rosário Moura <yves.moura@saude.ba.gov.br>
2022-10-31 22:21:04 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let modalQt = 1
+let cart = []
+let modalKey = 0
 
 const c = (el) => document.querySelector(el)
 const cs = (el) => document.querySelectorAll(el)
+
+// listagem das pizzas
 
 pizzaJson.map((item, index)=>{
   let pizzaItem = c('.models .pizza-item').cloneNode(true);
@@ -16,6 +20,7 @@ pizzaJson.map((item, index)=>{
     let key = e.target.closest('.pizza-item').getAttribute('data-key')
 
     modalQt = 1
+    modalKey = key
 
     c('.pizzaBig img').src = pizzaJson[key].img
     c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
@@ -41,8 +46,68 @@ pizzaJson.map((item, index)=>{
       c('.pizzaWindowArea').style.opacity = 1
     },200)
   })
-
-
-
-  c('.pizza-area').append( pizzaItem)   // adicionando as pizza no home
+   c('.pizza-area').append( pizzaItem)   // adicionando as pizza no home
 });
+
+
+  // eventos do modal 
+function closeModal(){
+  c('.pizzaWindowArea').style.opacity = 0
+  setTimeout(()=>{
+    c('.pizzaWindowArea').style.display = 'none'
+  },500)
+
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+  item.addEventListener('click', closeModal)
+})
+
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+  if( modalQt > 1){
+    modalQt --
+    c('.pizzaInfo--qt').innerHTML = modalQt
+}})
+
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+  modalQt ++
+  c('.pizzaInfo--qt').innerHTML = modalQt
+})
+
+cs('.pizzaInfo--size').forEach((size,sizeIndex)=>{
+  size.addEventListener('click', (e)=>{
+    c('.pizzaInfo--size.selected').classList.remove('selected') // remover todos
+    size.classList.add('selected') // selecionar apenas oq foi selecionado
+  })
+})
+
+
+// preenchimento no carinho de compra 
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'))
+    let identifier = pizzaJson[modalKey].id+'@'+size
+    let key = cart.findIndex((item) =>item.identifier == identifier)
+    if (key > -1){
+      cart[key].qt += modalQt
+    }else{  
+    cart.push({ 
+      identifier,  //adicionando no array itens selecionados no modal
+      id:pizzaJson[modalKey].id,
+      size,
+      qt:modalQt
+    })}
+    updateCard()
+    closeModal()
+})
+
+function updateCard(){
+  if(cart.length > 0){
+    c('aside').classList.add('show')
+    for ( let i in cart){
+      let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id)
+      console.log(pizzaItem)
+    }
+}else{  
+    c('aside').classList.remove('show')
+    }
+}
